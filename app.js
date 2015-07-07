9b2c928193baa344f2b8f8e63fa107aa2d27b50c
Dee Roberts <deerob4@gmail.com>
2015-07-07 23:10:22 +0100

    Remove app.js.

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/podge/*', function(req, res) {
-	res.send('Hello world!');
-});
-
-app.listen(5000);
-console.log('App listening on port 5000')

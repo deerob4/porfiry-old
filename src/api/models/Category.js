ccc98aabe8cc30a7d74d0cd1b92a9dfbb5144a7f
Dee Roberts <deerob4@gmail.com>
2015-09-23 11:24:21 +0100

    Update Category.js

--- src/api/models/Category.js
+++ src/api/models/Category.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
-
 import questionSchema from './Question';
 
-let categorySchema = new mongoose.Schema({
+const categorySchema = new mongoose.Schema({
   name: String,
   questions: [questionSchema]
 });

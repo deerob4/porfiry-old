2cfaa06bba97a1935924fea99da88e256c682007
Dee Roberts <deerob4@gmail.com>
2015-09-23 11:30:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,18 @@ import webpack from 'webpack';
 import config from './webpack.config';
 import apiRoutes from './src/api/index';
 
+// Begin connection to database.
 mongoose.connect('mongodb://localhost:27017/porfiry');
 
 let app = express();
 let compiler = webpack(config);
 
+// Initialise essential middleware.
 app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Set up development middleware.
 app.use(require('webpack-dev-middleware')(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath
@@ -22,10 +25,12 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
+// Set root route (pun!) to serve static index.html file.
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Set the /api endpoint to the route logic in apiRoutes.
 app.use('/api', apiRoutes);
 
 app.listen(app.get('port'), 'localhost', err => {

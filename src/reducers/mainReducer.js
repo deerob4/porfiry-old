96cbbb75f2ba10ad0589aad48a97f322ae170ef8
Dee Roberts <deerob4@gmail.com>
2015-09-29 15:08:31 +0100

    Create mainReducer.js

---
+++ src/reducers/mainReducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import {  CHANGE_HOUSE  } from 'constants/main';
+
+function house(state, action) {
+  switch (action.type) {
+    case CHANGE_HOUSE:
+      return action.house;
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({
+  house
+});

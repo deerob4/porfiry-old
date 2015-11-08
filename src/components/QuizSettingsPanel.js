ebc226c86a8e5896d6416a216cf4292015d97f67
Dee Roberts <deerob4@gmail.com>
2015-11-08 15:20:29 +0000

    Overhaul settings saving mechanism.

--- src/components/QuizSettingsPanel.js
+++ src/components/QuizSettingsPanel.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import settingsStyle from 'utils/settingsStyle';
 import Modal from 'react-modal';
+import Button from 'components/Button';
 
 class QuizSettingsPanel extends Component {
   static propTypes = {
@@ -13,20 +14,25 @@ class QuizSettingsPanel extends Component {
   constructor(props) {
     super(props);
 
-    this.editSetting = this.editSetting.bind(this);
+    this.saveSettings = this.saveSettings.bind(this);
   }
 
-  editSetting(setting) {
-    this.props.saveSettings(
-      setting,
-      this.refs[setting].value
-    );
+  saveSettings() {
+    this.props.saveSettings({
+      title: this.refs.title.value,
+      startDate: this.refs.startDate.value,
+      startTime: this.refs.startTime.value,
+      questionLength: this.refs.questionLength.value,
+      breakLength: this.refs.breakLength.value
+    });
+
+    this.props.closeSettings();
   }
 
   render() {
     return (
       <Modal isOpen={this.props.settingsAreOpen}
-            closeTimeoutMS={150}
+             closeTimeoutMS={150}
              onRequestClose={this.props.closeSettings}
              style={settingsStyle(this.props.house)}>
 
@@ -40,9 +46,8 @@ class QuizSettingsPanel extends Component {
                       Quiz name:
                    </label>
 
-                   <input onChange={this.editSetting.bind(this, 'title')}
-                          className={`${this.props.house}-input input-settings`}
-                          value={this.props.currentSettings.title}
+                   <input className={`${this.props.house}-input input-settings`}
+                          placeholder={this.props.currentSettings.title}
                           ref="title"
                           type="text" />
                  </div>
@@ -54,8 +59,7 @@ class QuizSettingsPanel extends Component {
                       Start day (the day it will be held):
                    </label>
 
-                   <input onChange={this.editSetting.bind(this, 'startDate')}
-                          className={`${this.props.house}-input input-settings`}
+                   <input className={`${this.props.house}-input input-settings`}
                           ref="startDate"
                           type="date" />
                  </div>
@@ -65,8 +69,7 @@ class QuizSettingsPanel extends Component {
                           Start time (the time on that day):
                    </label>
 
-                   <input onChange={this.editSetting.bind(this, 'startTime')}
-                          className={`${this.props.house}-input input-settings`}
+                   <input className={`${this.props.house}-input input-settings`}
                           ref="startTime"
                           type="time" />
                  </div>
@@ -78,10 +81,9 @@ class QuizSettingsPanel extends Component {
                           Question length (seconds):
                    </label>
 
-                   <input onChange={this.editSetting.bind(this, 'questionLength')}
-                          className={`${this.props.house}-input input-settings`}
+                   <input className={`${this.props.house}-input input-settings`}
                           ref="questionLength"
-                          value={this.props.currentSettings.questionLength}
+                          placeholder={this.props.currentSettings.questionLength}
                           type="number"
                           min="5" />
                  </div>
@@ -92,13 +94,29 @@ class QuizSettingsPanel extends Component {
                           Break length (minutes):
                    </label>
 
-                   <input onChange={this.editSetting.bind(this, 'breakLength')}
-                          className={`${this.props.house}-input input-settings`}
+                   <input className={`${this.props.house}-input input-settings`}
                           ref="breakLength"
-                          value={this.props.currentSettings.breakLength}
+                          placeholder={this.props.currentSettings.breakLength}
                           type="number"
                           min="1" />
                  </div>
+               </div>
+
+               <div className="row">
+                <div className="field col-md-6 setting-buttons">
+                  <Button clickEvent={this.saveSettings}
+                          customClass="create-quiz-button settings-save"
+                          text="Save settings"
+                          icon="save"
+                          house={this.props.house} />
+                </div>
+                <div className="field col-md-6 setting-buttons">
+                  <Button clickEvent={this.props.closeSettings}
+                          customClass="create-quiz-button settings-cancel"
+                          text="Cancel changes"
+                          icon="save"
+                          house={this.props.house} />
+                </div>
                </div>
              </form>
       </Modal>
--- src/containers/CreateQuizContainer.js
+++ src/containers/CreateQuizContainer.js
@@ -126,26 +126,28 @@ class CreateQuizContainer extends Component {
    */
   markCorrect(answerId, questionId, body, correct) {
     // Get the question that's currently marked as correct.
-    const currentlyCorrect = find(this.props.quiz.answers,
-      x => x.correct === true && x.questionId === questionId
+    const currentlyCorrect = find(this.props.quiz.answers, x =>
+      x.correct === true && x.questionId === questionId
     );
 
     // Ensure they don't try to mark the current answer.
     if (answerId !== currentlyCorrect.id) {
       // Update the state tree to make the currently correct answer incorrect,
       // so only one answer at a time can be correct at a time.
-      this.props.dispatch(actions.editAnswer(
-        currentlyCorrect.id,
-        currentlyCorrect.body,
-        false
-      ));
+      this.editAnswer(currentlyCorrect.id, currentlyCorrect.body, false);
       // Update the state tree to mark the newly selected answer as correct.
-      this.props.dispatch(actions.editAnswer(answerId, body, correct));
+      this.editAnswer(answerId, body, correct);
       // Update the container to state to cause a re-render of the interface.
       this.setState({ currentQuestion: questionId });
     }
   }
 
+  /**
+   * Edits a question answer with new properties.
+   * @param  {Number} id      ID of answer to edit/
+   * @param  {String} body    New body of answer.
+   * @param  {Bool} correct   New correct status of answer.
+   */
   editAnswer(id, body, correct) {
     this.props.dispatch(actions.editAnswer(
       id,
@@ -185,26 +187,34 @@ class CreateQuizContainer extends Component {
    * @param  {String} setting The setting that has been modified.
    * @param  {String|Numbe}r value   The new setting data.
    */
-  saveSettings(setting, value) {
-    if (value.length) {
-      switch (setting) {
-        case 'title':
-          this.props.dispatch(actions.updateTitle(value));
-          break;
-        case 'startDate':
-          this.props.dispatch(actions.updateStartDate(value));
-          break;
-        case 'startTime':
-          this.props.dispatch(actions.updateStartTime(value));
-          break;
-        case 'questionLength':
-          this.props.dispatch(actions.updateQuestionLength(parseInt(value)));
-          break;
-        case 'breakLength':
-          this.props.dispatch(actions.updateBreakLength(parseInt(value)));
-          break;
-        default:
-          return value;
+  saveSettings(settings) {
+    // Loop through every setting key.
+    for (let setting in settings) {
+      // Get the value set in the settings panel.
+      let value = settings[setting];
+      // If they've made any changes compared to the value we have
+      // stored in the state tree.
+      if (value.length && value !== this.props.quiz.settings[setting]) {
+        // Call the correct action to update the state.
+        switch (setting) {
+          case 'title':
+            this.props.dispatch(actions.updateTitle(value));
+            break;
+          case 'startDate':
+            this.props.dispatch(actions.updateStartDate(value));
+            break;
+          case 'startTime':
+            this.props.dispatch(actions.updateStartTime(value));
+            break;
+          case 'questionLength':
+            this.props.dispatch(actions.updateQuestionLength(parseInt(value)));
+            break;
+          case 'breakLength':
+            this.props.dispatch(actions.updateBreakLength(parseInt(value)));
+            break;
+          default:
+            return value;
+        }
       }
     }
   }

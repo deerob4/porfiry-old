6e7baa9b56aa056039d492ad2e694e47143b467b
Dee Roberts <deerob4@gmail.com>
2016-01-29 15:57:32 +0000

    Add redux-devtools-dock-monitor.

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { createDevTools } from 'redux-devtools';
 import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
+import FilterMonitor from 'redux-devtools-filter-actions';
 
 const DevTools = createDevTools(
   <DockMonitor toggleVisibilityKey="ctrl-h"
                changePositionKey="ctrl-q"
                defaultSize={0.12}>
-    <LogMonitor />
+    <FilterMonitor blacklist={['DECREMENT_TIME_LEFT']}>
+      <LogMonitor />
+    </FilterMonitor>
   </DockMonitor>
 );
 

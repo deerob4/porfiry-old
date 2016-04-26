73f43caad23cc519cf95c8dc82545e6cdd77ad5f
Dee Roberts <deerob4@gmail.com>
2016-04-26 09:39:54 +0100

    Delete formatTime.js

--- src/utils/formatTime.js
+++
@@ -1,9 +0,0 @@
-function formatTime(ms) {
-  const seconds = (ms / 1000) % 60;
-  const minutes = (ms / 60000) % 60;
-  const hours = (ms / 3600000) % 60;
-
-  return { hours, minutes, seconds };
-}
-
-export default formatTime;

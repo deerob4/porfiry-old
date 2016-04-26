e14df3688f1ccf8f1b57d10ca7241aadb924749b
Dee Roberts <deerob4@gmail.com>
2016-04-26 09:39:19 +0100

    Delete isQuizReady.js

--- src/utils/isQuizReady.js
+++
@@ -1,8 +0,0 @@
-import moment from 'moment';
-
-function isQuizReady(quiz) {
-  let minutesToStart = moment(quiz.startDate).diff(moment(), 'minutes');
-  return minutesToStart >= -5 && minutesToStart < 30;
-}
-
-export default isQuizReady;
